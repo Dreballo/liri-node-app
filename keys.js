22ad7da7660dd1ecb93280773bdd26f341971801
Andre Seballlo <andreseballlo@Stacys-MacBook-Air.local>
2017-03-30 21:47:25 -0400

    working liri bot, needs bug fix for do-what-it-says command

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'k33zQ3vIOVJe3O93mVXdizlAf',
+  consumer_secret: 'FZtSZy3ngWeQjyMZCItWODKJQQfIxZlK3EohCL6K2u2Calih23',
+  access_token_key: '26777205-tunLZJDZnQop91NfRiouod8yGtMjUVfZT7WO3QDe1',
+  access_token_secret: 'GAf5xX0tLhi9vK3GM4BIsII2OGeuK1PbKiUoJbGVVGbc1',
+}
